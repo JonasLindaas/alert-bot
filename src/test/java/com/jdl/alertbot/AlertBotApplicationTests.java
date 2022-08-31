203cfb260cefaa7aa4a9be191a763b0f5d6c7dc0
JonasLindaas <jonas.drageset.lindaas@gmail.com>
2022-08-31 15:22:37 +0200

    Init

---
+++ src/test/java/com/jdl/alertbot/AlertBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdl.alertbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
