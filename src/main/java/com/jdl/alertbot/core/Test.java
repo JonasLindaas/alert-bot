2af50343bac89bec62c186835af5b84e36fb9208
JonasLindaas <jonas.drageset.lindaas@gmail.com>
2022-09-01 21:29:51 +0200

    Added Javacord and wrote simple test.

---
+++ src/main/java/com/jdl/alertbot/core/Test.java
@@ -0,0 +1,24 @@
+package com.jdl.alertbot.core;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+
+    @Autowired
+    public Test() {
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken("")
+                .login().join();
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("ping")) {
+                event.getChannel().sendMessage("pong");
+            }
+        });
+    }
+
+}
