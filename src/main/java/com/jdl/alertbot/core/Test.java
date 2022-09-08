52015df1d5f3d67ab401b2c7d1626035841be186
JonasLindaas <jonas.drageset.lindaas@gmail.com>
2022-09-08 15:07:11 +0200

    Test for environmental variable fixed (Hopefully).

--- src/main/java/com/jdl/alertbot/core/Test.java
+++ src/main/java/com/jdl/alertbot/core/Test.java
@@ -1,11 +1,7 @@
 package com.jdl.alertbot.core;
 
-import org.javacord.api.DiscordApi;
-import org.javacord.api.DiscordApiBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 @Component
 public class Test {
@@ -22,19 +18,8 @@ public class Test {
 //            }
 //        });
 
-        // Testing to get variables from YAML pipeline using environmental variables
-        // Doc mentioned that variables are uppercased when passed to environment, hence test 2
-
-        System.out.println("Test 1:" + System.getenv("env-test-variable"));
-        System.out.println("Test 2:" + System.getenv("$ENV_TEST_VARIABLE"));
-
-        // Neither worked, so I'm trying a different approach:
-        // Get all environmental variables and print them.
-        Map<String, String> envMap = System.getenv();
-
-        for (String envName : envMap.keySet()) {
-            System.out.format("%s = %s%n", envName, envMap.get(envName));
-        }
+        // Seems the '$' was not needed, this should hopefully work:
+        System.out.println("Test 2:" + System.getenv("ENV_TEST_VARIABLE"));
 
     }
 
