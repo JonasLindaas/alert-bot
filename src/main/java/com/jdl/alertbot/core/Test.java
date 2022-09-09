0305b10619990eb2c204dea3ea9cd801b18412e9
JonasLindaas <jonas.drageset.lindaas@gmail.com>
2022-09-09 13:18:16 +0200

    Full CI/CD test.
    With this I'm testing that the entire CI/CD system works as planned, using a very simple Discord Bot that simply responds with pong when someone types ping.

--- src/main/java/com/jdl/alertbot/core/Test.java
+++ src/main/java/com/jdl/alertbot/core/Test.java
@@ -1,5 +1,7 @@
 package com.jdl.alertbot.core;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,17 +10,17 @@ public class Test {
 
     @Autowired
     public Test() {
-//        DiscordApi api = new DiscordApiBuilder()
-//                .setToken("")
-//                .login().join();
-//
-//        api.addMessageCreateListener(event -> {
-//            if (event.getMessageContent().equalsIgnoreCase("ping")) {
-//                event.getChannel().sendMessage("pong");
-//            }
-//        });
+        String APIKeyToken = System.getenv("DISCORD_BOT_TOKEN");
 
-        System.out.println("Test:" + System.getenv("ENV_TEST_VARIABLE"));
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken(APIKeyToken)
+                .login().join();
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("ping")) {
+                event.getChannel().sendMessage("pong");
+            }
+        });
 
     }
 
